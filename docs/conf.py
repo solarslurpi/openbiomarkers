45bdc182f7384675b43499ecf29344f55006d884
Happyday <happyday.mjohnson@gmail.com>
2020-12-26 04:55:04 -0800

    changed them to sphinx-rtd

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import sphinx_rtd_theme
+import os
+import sys
+# Source path is off the parent directory
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.append('firmware')
 
 
 # -- Project information -----------------------------------------------------
@@ -20,14 +23,15 @@
 project = 'openbiomarkers'
 copyright = '2020, happyday'
 author = 'happyday'
-today = ''
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
 extensions = [
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,7 +48,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/index.rst
+++ docs/index.rst
@@ -10,6 +10,7 @@ Welcome to openbiomarkers's documentation!
    :maxdepth: 2
    :caption: Contents:
 
+
    heartrate_bloodoxygen
 
 
